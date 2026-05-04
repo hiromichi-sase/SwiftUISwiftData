82b34c73b1465025aa092ac0a7d09588e746228b
Hiro <hiromichi.sase@gmail.com>
2026-05-04 18:16:54 +0900

    Itemのpropertyを変更

--- SwiftUISwiftData/ContentView.swift
+++ SwiftUISwiftData/ContentView.swift
@@ -17,9 +17,9 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Item at \(item.createdAt, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text(item.createdAt, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -41,7 +41,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
+            let newItem = Item(title: "", content: "", createdAt: Date(), updatedAt: Date(), order: items.count + 1)
             modelContext.insert(newItem)
         }
     }
--- SwiftUISwiftData/Item.swift
+++ SwiftUISwiftData/Item.swift
@@ -10,9 +10,17 @@ import SwiftData
 
 @Model
 final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    var title: String
+    var content: String
+    var createdAt: Date
+    var updatedAt: Date
+    var order: Int
+
+    init(title: String, content: String, createdAt: Date, updatedAt: Date, order: Int) {
+        self.title = title
+        self.content = content
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.order = order
     }
 }
