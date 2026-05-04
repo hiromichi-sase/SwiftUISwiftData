50a099ea9d8996cfe14350ca52099ec6b17eac73
Hiro <hiromichi.sase@gmail.com>
2026-05-04 16:34:03 +0900

    Initial Commit

---
+++ SwiftUISwiftData/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftUISwiftData
+//
+//  Created by Hiromichi Sase on 2026/05/04.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftUISwiftDataTests/SwiftUISwiftDataTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUISwiftDataTests.swift
+//  SwiftUISwiftDataTests
+//
+//  Created by Hiromichi Sase on 2026/05/04.
+//
+
+import Testing
+@testable import SwiftUISwiftData
+
+struct SwiftUISwiftDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
