14e1acea468cd95979899e9584ce1162f810638a
Hiro <hiromichi.sase@gmail.com>
2026-06-11 21:58:20 +0900

    編集画面テスト リファクタリング(addMemo)

--- SwiftUISwiftDataTests/EditMemoViewModelTests.swift
+++ SwiftUISwiftDataTests/EditMemoViewModelTests.swift
@@ -13,7 +13,9 @@ struct EditMemoViewModelTests {
 
     @Test func addMemo() async throws {
         let dependency = Dependency()
-        let memo = Memo(title: "Test Title", content: "Test Memo")
+        let addedTitle = "Added Title"
+        let addedContent = "Added Content"
+        let memo = Memo(title: addedTitle, content: addedContent)
         try await dependency.testTarget.add(memo)
 
         guard let newMemo = await dependency.memoRepository.memos().first else {
@@ -21,8 +23,8 @@ struct EditMemoViewModelTests {
         }
 
         await #expect(dependency.memoRepository.memos().count == 1)
-        #expect(newMemo.title == "Test Title")
-        #expect(newMemo.content == "Test Memo")
+        #expect(newMemo.title == addedTitle)
+        #expect(newMemo.content == addedContent)
         #expect(newMemo.createdAt == newMemo.updatedAt)
         #expect(newMemo.order == 1)
         dependency.removeUserDefaults()
