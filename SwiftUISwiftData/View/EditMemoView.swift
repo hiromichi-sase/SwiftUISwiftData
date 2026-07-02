456dd35d144c920c4ae7909306456141293fdaed
Hiro <hiromichi.sase@gmail.com>
2026-07-02 17:11:19 +0900

    編集画面 titleにInputViewを適用

--- SwiftUISwiftData/View/EditMemoView.swift
+++ SwiftUISwiftData/View/EditMemoView.swift
@@ -55,6 +55,8 @@ struct EditMemoView: View {
     private var error: Error?
     @State
     private var currentAlert: AlertType?
+    @FocusState
+    private var inputViewFocus: Bool
     /// ナビゲーションパスの状態変数。
     @State
     var path = NavigationPath()
@@ -74,7 +76,16 @@ struct EditMemoView: View {
         NavigationStack(path: $path) {
             VStack(spacing: 4) {
                 if showTitleView {
-                    titleView
+                    InputView(
+                        text: $title,
+                        focus: _inputViewFocus,
+                        placeholder: "Input keywords to search by title",
+                        submitLabel: .done,
+                        icon: .search
+                    ) {
+                        showTitleView = false
+                        title = titleToStore
+                    }
                 }
                 contentView
                 if viewModel.getShowInfo(), !showTitleView {
@@ -115,22 +126,6 @@ struct EditMemoView: View {
             }
             .toast(message: $toastMessage)
         }
-    }
-
-    /// タイトル編集ビュー。
-    private var titleView: some View {
-        HStack(spacing: 8) {
-            TextField("Input Title", text: $title)
-                .padding(6)
-                .border(.primary)
-                .focused($textFieldFocus)
-                .submitLabel(.done)
-                .onSubmit {
-                    titleToStore = title
-                    showTitleView = false
-                }
-        }
-        .padding(.bottom, 4)
     }
 
     /// 内容編集ビュー。
@@ -193,16 +188,12 @@ struct EditMemoView: View {
     @ViewBuilder
     private var toolbarItemTopBarTrailing: some View {
         Button("Rename", systemImage: "rectangle.and.pencil.and.ellipsis") {
-            showTitleView.toggle()
-            if showTitleView {
-                DispatchQueue.main.async {
-                    textFieldFocus = true
-                }
-            }
-            else {
-                title = titleToStore
+            showTitleView = true
+            DispatchQueue.main.async {
+                inputViewFocus = true
             }
         }
+        .disabled(showTitleView)
         .keyboardShortcut("t", modifiers: [.command])
         Button("Save", systemImage: "square.and.pencil") {
             if let memo {
